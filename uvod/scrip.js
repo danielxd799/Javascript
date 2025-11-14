c1c7007ec0191b2a7828cadf62115cbd35a815f5
Daniel Dítě <ditedaniel19@gmail.com>
2025-11-14 14:47:19 +0100

    hodina 1-2 js

---
+++ uvod/scrip.js
@@ -0,0 +1,26 @@
+// HELLO WORLD
+/*
+console.log("Hello world");
+console.error("Hello world");
+console.warn("Hello world");
+ 
+//window alert
+alert("Hello world");
+console.info("Hello world");
+ 
+//console.tabel
+console.table(["hodnota1", "hodnota2", "hodnotaN"]);
+*/
+//input
+
+function nacti(){
+    let a = document.getElementById("1").value;
+    let cislo = Number(a);
+    console.log(cislo);
+}
+ 
+function nacti2(){
+    let b = document.getElementById("2").value;
+    let cislo2 = Number(b);
+    console.log(cislo2);
+}
