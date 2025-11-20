aa4214259a448721852dfd1596335b1f08d59344
Daniel Dítě <ditedaniel19@gmail.com>
2025-11-20 17:39:33 +0100

    js ukoly

---
+++ ukol2/script.js
@@ -0,0 +1,11 @@
+/*****************************************************************/
+// Uloha: Spočítej součet dvou čísel zadaných uživatelem
+// Požádej uživatele o dvě čísla pomocí `prompt()` a vypiš jejich součet do konzole.
+ 
+// zde bude tvoje řešení
+let cislo1 = prompt("Zadej prvni cislo: ");
+let cislo2 = prompt("Zadej druhe cislo: ");
+let num1 = Number(cislo1);
+let num2 = Number(cislo2);
+console.log(num1 + num2);
+/*****************************************************************/
